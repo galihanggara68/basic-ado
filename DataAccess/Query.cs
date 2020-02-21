2467e41d52811d2bc6637d7d62606b700ad4eab3
Galih Anggara <galihanggara68@gmail.com>
2020-02-21 15:45:23 +0700

    Add files via upload

---
+++ DataAccess/Query.cs
@@ -0,0 +1,57 @@
+﻿using FirstApp.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.DataAccess
+{
+    class Query
+    {
+        private SqlConnection connection;
+
+        public Query()
+        {
+            Connector connector = new Connector();
+            connection = connector.GetConnection();
+        }
+
+        public List<Dictionary<string, string>> SelectGetAll()
+        {
+            List<Dictionary<string, string>> employees = new List<Dictionary<string, string>>();
+            connection.Open();
+            SqlCommand query = new SqlCommand("select employee_id, first_name, last_name from hr.employees", connection);
+            SqlDataReader reader = query.ExecuteReader();
+            while(reader.Read())
+            {
+                Dictionary<string, string> employee = new Dictionary<string, string>();
+                employee.Add("employee_id", reader["employee_id"].ToString());
+                employee.Add("first_name", reader["first_name"].ToString());
+                employee.Add("last_name", reader["last_name"].ToString());
+
+                employees.Add(employee);
+            }
+            connection.Close();
+            return employees;
+        }
+
+        public Dictionary<string, string> GetOne(int employeeId)
+        {
+            Dictionary<string, string> employee = new Dictionary<string, string>();
+            connection.Open();
+
+            SqlCommand query = new SqlCommand("select employee_id, first_name, last_name from hr.copy_emp where employee_id = " + employeeId, connection);
+            SqlDataReader reader = query.ExecuteReader();
+            reader.Read();
+            employee.Add("employee_id", reader["employee_id"].ToString());
+            employee.Add("first_name", reader["first_name"].ToString());
+            employee.Add("last_name", reader["last_name"].ToString());
+
+            connection.Close();
+
+            return employee;
+        }
+    }
+}
---
+++ Utility/Connector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.Utility
+{
+    class Connector
+    {
+        private SqlConnection connection;
+
+        public Connector()
+        {
+            connection = new SqlConnection(@"Server=.\SQLEXPRESS;Database=HR;Trusted_Connection=true");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
